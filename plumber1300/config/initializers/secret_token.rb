0069dd8bcce903b54a3339616476786e8c1704e7
ssilva2012 <ngcssilva@gmail.com>
2012-09-28 21:31:24 +1000

    Init version of RoR without any change

---
+++ plumber1300/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plumber1300::Application.config.secret_token = 'e3b9fd8ef48004a38a7062f075dcfcf1d81f19aa02632e62dcda8f1895245a9ec5a6ec8abf4a45e579e6c83fea027e4779bf4ae15c553f51205c8013b976fe39'
